9cd1f72583e1d67067e22d73be0f681abbfa6c9f
Ankush Joshi <ankujoshi@deloitte.com>
2022-11-22 01:29:26 +0530

    - changes

--- app/src/main/java/com/example/appshortcuts/Shortcuts.kt
+++ app/src/main/java/com/example/appshortcuts/Shortcuts.kt
@@ -1,56 +1,46 @@
 package com.example.appshortcuts
 
 import android.content.Context
-import android.content.Intent
 import android.content.pm.ShortcutInfo
 import android.content.pm.ShortcutManager
-import android.graphics.drawable.Icon
-import android.net.Uri
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.*
+import androidx.core.content.pm.ShortcutInfoCompat
+import androidx.core.content.pm.ShortcutManagerCompat
+import com.example.data.AppShortcutInfo
 
 const val shortcut_website_id = "id_website"
 const val shortcut_messages_id = "id_messages"
+const val shortcut_settings_id = "id_settings"
 const val shortcut_call_id = "id_call"
 
 //Requires api level 25
 @RequiresApi(Build.VERSION_CODES.N_MR1)
 object Shortcuts {
 
-    fun setUp(context: Context) {
-        val shortcutManager =
-            getSystemService<ShortcutManager>(context, ShortcutManager::class.java)
-
-
-        val shortcut = ShortcutInfo.Builder(context, shortcut_website_id)
-            .setShortLabel("Website")
-            .setLongLabel("Open the website")
-            .setIcon(Icon.createWithResource(context, R.drawable.ic_website))
-            .setIntent(
-                Intent(
-                    Intent.ACTION_VIEW,
-                    Uri.parse("https://www.youtube.com/codepalace")
-                )
-            )
+    fun setUpSingleShortcut(context: Context, appShortcutInfo: AppShortcutInfo) {
+        val shortcut = ShortcutInfoCompat.Builder(context, appShortcutInfo.id)
+            .setShortLabel(appShortcutInfo.label)
+            .setLongLabel(appShortcutInfo.longLabel)
+            .setIcon(appShortcutInfo.icon)
+            .setIntent(appShortcutInfo.intent)
             .build()
-
-        //Create an array of intents to create a more fluent user experience in the back stack
-        val intents = arrayOf(
-            Intent(Intent.ACTION_VIEW, null, context, MainActivity::class.java),
-            Intent(Intent.ACTION_VIEW, null, context, Messages::class.java)
-        )
-
-        val shortcut2 = ShortcutInfo.Builder(context, shortcut_messages_id)
-            .setShortLabel("Messages")
-            .setLongLabel("Open messages")
-            .setIcon(Icon.createWithResource(context, R.drawable.ic_messages))
-            .setIntents(intents)
-            .build()
-
-
-        shortcutManager!!.dynamicShortcuts = listOf(shortcut, shortcut2)
+        ShortcutManagerCompat.pushDynamicShortcut(context, shortcut)
     }
 
+    fun setupMultipleShortcuts(context: Context, list: List<AppShortcutInfo>) {
+        val shortcutManager = getSystemService(context, ShortcutManager::class.java)
+        val listOfShortcutInfo = ArrayList<ShortcutInfo>()
+        for (appShortcutInfo in list) {
+            val shortcutInfo = ShortcutInfo.Builder(context, appShortcutInfo.id)
+                .setShortLabel(appShortcutInfo.label)
+                .setLongLabel(appShortcutInfo.longLabel)
+                .setIcon(appShortcutInfo.icon.toIcon())
+                .setIntent(appShortcutInfo.intent)
+                .build()
+            listOfShortcutInfo.add(shortcutInfo)
+        }
+        shortcutManager!!.dynamicShortcuts = listOfShortcutInfo
+    }
 }
